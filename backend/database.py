db32a2cdda64f77b6bcfee0fb1764ded780879cc
Ritik Raj <ritikraj620071@gmail.com>
2026-05-29 00:26:44 +0530

    fixed database config final

--- backend/database.py
+++ backend/database.py
@@ -1,21 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import os
-SQLALCHEMY_DATABASE_URL = "sqlite:///./failsafe.db"
-# For rapid development, we will use SQLite. 
-# Swap to the PostgreSQL URL below when you are ready for production.
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./failsafe.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/failsafe_db"
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# connect_args is only needed for SQLite
+SQLALCHEMY_DATABASE_URL = "sqlite:///./failsafe.db"
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
 def get_db():
